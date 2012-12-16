61d3cf0c2224ffe49b04a5726c5b1923deac51b4
John Abrahamsen <jab@NOR128.(none)>
2012-12-16 13:38:14 +0100

    dstr and dstr_list with base functions added

---
+++ src/dstr.c
@@ -0,0 +1,285 @@
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+
+#include "dstr.h"
+
+/******************************** DYNAMIC STRING ********************************/
+
+static int alloc_more(dstr* str, int sz)
+{
+    int more_mem;
+
+    more_mem = (int)(str->allocd_mem + (sz * sizeof(char))) * str->growth_rate;
+    str->allocd_mem = more_mem;
+    str->buf = realloc(str->buf, more_mem);
+    if (!str->buf)
+        return 0;
+    return 1;
+}
+
+static int can_hold(const dstr *str, int chars)
+{
+    if (!str->buf)
+        return 0;
+    else
+        return (chars < str->allocd_mem);
+}
+
+dstr *dstr_new()
+{
+    dstr *str = malloc(sizeof(dstr));
+
+    if (!str)
+        return 0;
+    str->end = 0;
+    str->buf = 0;
+    str->growth_rate = 2;
+    str->ref = 1;
+    return str;
+}
+
+dstr *dstr_with_initial(const char *initial)
+{
+    dstr *str = malloc(sizeof(dstr));
+
+    if (!str)
+        return 0;
+    str->end = strlen(initial);
+    str->growth_rate = 2;
+    str->allocd_mem = str->end;
+    str->buf = strdup(initial);
+    if (!str->buf)
+        return 0;
+    str->ref = 1;
+    return str;
+}
+
+dstr *dstr_with_prealloc(unsigned int sz)
+{
+    dstr *str = malloc(sizeof(dstr));
+    int pre_alloc_mem = sz + 1;
+
+    if (!str)
+        return 0;
+    str->end = 0;
+    str->buf = 0;
+    str->growth_rate = 2;
+    if (!alloc_more(str, pre_alloc_mem))
+        return 0;
+    str->buf[0] = '\0';
+    str->ref = 1;
+    return str;
+}
+
+int dstr_compact(dstr *str)
+{
+    if (str->allocd_mem < str->end){
+        str->buf = realloc(str->buf, str->end + 1);
+        if (str->buf)
+            return 1;
+    }
+    return 0;
+}
+
+int dstr_append(dstr* dest, const dstr* src)
+{
+    int total = src->end + dest->end;
+    if (!can_hold(dest, total + 1)){
+        if (!alloc_more(dest, total))
+            return 0;
+    }
+    strcpy(dest->buf+dest->end, src->buf);
+    dest->end = total;
+    return 1;
+}
+
+int dstr_append_cstr(dstr* dest, const char *src)
+{
+    int total = strlen(src) + dest->end;
+    if (!can_hold(dest, total + 1)){
+        if (!alloc_more(dest, total))
+            return 0;
+    }
+    strcpy(dest->buf+dest->end, src);
+    dest->end = total;
+    return 1;
+}
+
+int dstr_append_decref(dstr* dest, dstr* src)
+{
+    if (dstr_append(dest, src)){
+        dstr_decref(src);
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+dstr *dstr_copy(const dstr *copy)
+{
+    int rc;
+    dstr* str = dstr_with_prealloc(copy->end + 1);
+    if (!str)
+        return 0;
+    rc = dstr_append(str, copy);
+    if (!rc)
+        return 0;
+    return str;
+}
+
+void dstr_clear(dstr *str)
+{
+    int i = str->allocd_mem;
+    for (; i >= 0; i--){
+        str->buf[i] = 0;
+    }
+    str->end = 0;
+}
+
+int dstr_print(const dstr *src)
+{
+    return printf("%s", src->buf);
+}
+
+void dstr_growth_rate(dstr *dest, int rate)
+{
+    dest->growth_rate = rate;
+}
+
+/***************************** DYNAMIC STRING LIST  ******************************/
+
+dstr_list *dstr_list_new()
+{
+    dstr_list *list = malloc(sizeof(dstr_list));
+    if (!list)
+        return 0;
+    list->first = 0;
+    list->last = 0;
+    list->ref = 1;
+    return list;
+}
+
+int dstr_list_add(dstr_list *list, dstr *str)
+{
+    dstr_link_t *link;
+
+    link = calloc (1, sizeof (dstr_link_t));
+    if (!link)
+        return 0;
+
+    link->str = str;
+    dstr_incref(str);
+
+    if (list->last) {
+        list->last->next = link;
+        link->prev = list->last;
+        list->last = link;
+    } else {
+        list->first = link;
+        list->last = link;
+    }
+    return 1;
+}
+
+int dstr_list_add_decref(dstr_list *dest, dstr *str)
+{
+    int rc = dstr_list_add(dest, str);
+    if (rc)
+        dstr_decref(str);
+    return rc;
+}
+
+void dstr_list_remove(dstr_list *list, dstr_link_t *link)
+{
+    dstr_link_t * prev;
+    dstr_link_t * next;
+
+    prev = link->prev;
+    next = link->next;
+    if (prev){
+        if (next) {
+            prev->next = next;
+            next->prev = prev;
+        } else {
+            prev->next = 0;
+            list->last = prev;
+        }
+    } else {
+        if (next){
+            next->prev = 0;
+            list->first = next;
+        } else {
+            list->first = 0;
+            list->last = 0;
+        }
+    }
+
+    dstr_decref(link->str);
+    free(link);
+}
+
+void dstr_list_traverse(dstr_list * list, void (*callback)(dstr *))
+{
+    dstr_link_t *link;
+
+    for (link = list->first; link; link = link->next){
+        callback((void *) link->str);
+    }
+}
+
+void dstr_list_traverse_reverse (dstr_list *list, void (*callback)(dstr *))
+{
+    dstr_link_t * link;
+
+    for (link = list->last; link; link = link->prev) {
+        callback ((void *) link->str);
+    }
+}
+
+void dstr_list_traverse_delete (dstr_list * list, int (*callback)(dstr *))
+{
+    dstr_link_t *link;
+
+    for (link = list->first; link; link = link->next) {
+        if (callback((void *) link->str)) {
+            dstr_list_remove(list, link);
+        }
+    }
+}
+
+void dstr_list_decref (dstr_list *list)
+{
+    dstr_link_t *link;
+    dstr_link_t *next;
+
+    list->ref--;
+    if (!list->ref){
+        for (link = list->first; link; link = next){
+            next = link->next;
+            dstr_decref(link->str);
+            free(link);
+        }
+        free(list);
+    }
+}
+
+dstr *dstr_list_to_dstr(const char *sep, dstr_list *list)
+{
+    dstr *str = dstr_new();
+    dstr_link_t *link;
+
+    if (sep){
+        for (link = list->first; link; link = link->next){
+            dstr_append(str, link->str);
+            if (link->next)
+                dstr_append_cstr(str, sep);
+        }
+    } else {
+        for (link = list->first; link; link = link->next){
+            dstr_append(str, link->str);
+        }
+    }
+
+    return str;
+}
---
+++ src/dstr.h
@@ -0,0 +1,225 @@
+/*  Reference counted dynamic string and string list.
+    Copyright 2012 John Abrahamsen <jhnabrhmsn@gmail.com>
+
+    Permission is hereby granted, free of charge, to any person obtaining
+    a copy of this software and associated documentation files (the
+    "Software"), to deal in the Software without restriction, including
+    without limitation the rights to use, copy, modify, merge, publish,
+    distribute, sublicense, and/or sell copies of the Software, and to
+    permit persons to whom the Software is furnished to do so, subject to
+    the following conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+    LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+    OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+
+#ifndef _DSTR_H
+#define _DSTR_H 1
+
+typedef struct dstr{
+    char* buf;
+    unsigned int allocd_mem;
+    unsigned int end;
+    unsigned int ref;
+    unsigned int growth_rate;
+} dstr;
+
+typedef struct dstr_link_t {
+    dstr *str;
+    struct dstr_link_t *prev;
+    struct dstr_link_t *next;
+} dstr_link_t;
+
+typedef struct dstr_list {
+    dstr_link_t *first;
+    dstr_link_t *last;
+    int ref;
+} dstr_list;
+
+/********************* DYNAMIC STRING PUBLIC API ******************************/
+
+/**
+ * dstr_new create a new dynamic string object.
+ * @return a new dynamic string with no buffer allocated.
+ */
+dstr *dstr_new();
+
+/**
+ * dstr_with_initial create a new dynamic string object filled with
+ * initial C string.
+ * @param initial Initial string to copy into dynamic string.
+ * @return a new dynamic string with copied initial string.
+ */
+dstr *dstr_with_initial(const char *initial);
+
+/**
+ * dstr_with_prealloc create a new dynamic string object with
+ * pre allocated space.
+ * @param sz Amount of charaters to pre-allocate for.
+ * @return a new dynamic string with pre-allocated space.
+ */
+dstr *dstr_with_prealloc(unsigned int sz);
+
+/**
+ * dstr_to_cstr_const returns pointer to internal data.
+ * @note When the dynamic string is changed the data of the pointer is also changed.
+ * @param str Source dynamic string to return C string from.
+ * @return Zero terminated C string from dynamic string.
+ */
+const char *dstr_to_cstr_const(const dstr* str){ return str->buf; }
+
+/**
+ * dstr_decref decreases reference to the dynamic string.
+ * If no more references is found, the string is free'd.
+ * @param str The dynamic string to decrease reference for.
+ */
+void dstr_decref(dstr *str)
+{
+    str->ref--;
+    if (!str->ref){
+        free(str->buf);
+        free(str);
+    }
+}
+
+/**
+ * dstr_incref increases reference to the dynamic string.
+ * @param str The dynamic string to increase reference for.
+ */
+void dstr_incref(dstr *str){ str->ref++; }
+
+/**
+ * dstr_append appends a dynamic string to a dynamic string.
+ * @param dest Destination string to append to.
+ * @param src Source string to append.
+ * @return 0 on failure, 1 on success.
+ * @see dstr_append_decref for a reference stealing implementation.
+ */
+int dstr_append(dstr* dest, const dstr* src);
+
+/**
+ * dstr_append_decref appends a dynamic string to a dynamic string.
+ * This function also steals one reference attached to the source string.
+ * @param dest Destination string to append to.
+ * @param src Source string to append.
+ * @return 0 on failure, 1 on success.
+ * @see dstr_append for a non reference stealing implementation.
+ */
+int dstr_append_decref(dstr* dest, dstr* src);
+
+/**
+ * dstr_append_cstr appends a C string to a dynamic string.
+ * @param dest Destination string to append to.
+ * @param src Source C string to append.
+ * @return 0 on failure, 1 on success.
+ */
+int dstr_append_cstr(dstr* dest, const char *src);
+
+/**
+ * dstr_copy creates a copy of a dynamic string object.
+ * @param copy The dynamic string to copy.
+ * @return A independent copy of the source dynamic string, or 0 on allocation failure.
+ */
+dstr *dstr_copy(const dstr *copy);
+
+/**
+ * dstr_clear clears contents of a dynamic string.
+ * @param str  The dynamic string to clear.
+ * @note Will not modify reference nor free memory.
+ * @see dstr_compact for a deallocation of unused space.
+ */
+void dstr_clear(dstr *str);
+
+/**
+ * dstr_compact compacts the memory used by a string.
+ * @param str The dynamic string object to compact.
+ * @return 0 on failure, 1 on success.
+ * @note Not needed unless you are going from a really big string to a very small one.
+ */
+int dstr_compact(dstr *str);
+
+/**
+ * dstr_growth_rate modify the growth rate of memory when doing new allocations.
+ * Higher rate means more memory potentially lost, but string concatination speeds up.
+ * Lower rate means the opposite.
+ * @param dest The dynamic string to modify.
+ * @param rate The rate that required memory is multiplied by when allocating (to avoid thrasing).
+ */
+void dstr_growth_rate(dstr *dest, int rate);
+
+/********************* DYNAMIC STRING LIST PUBLIC API *************************/
+
+/**
+ * dstr_list_new creates a new referenced counted list for dynamic strings.
+ * @return initialized dynamic string list. If memory could not be allocated, 0 is returned.
+ */
+dstr_list *dstr_list_new();
+
+/**
+ * dstr_list_add Add a dynamic string to a list.
+ * One reference is added to the dynamic string. Which will be removed when the
+ * string is removed from the list or the lists has no more references.
+ * @param list The list to add the dynamic string to.
+ * @param str The string to add to string list.
+ * @return 0 on failure, 1 on success.
+ */
+int dstr_list_add(dstr_list *list, dstr *str);
+
+/**
+ * dstr_list_add Add a dynamic string to a list.
+ * No reference is added. Nonetheless one reference will be removed when the
+ * string is removed from the list or the lists has no more references.
+ * @param list The list to add the dynamic string to.
+ * @param str The string to add to string list.
+ * @return 0 on failure, 1 on success.
+ */
+int dstr_list_add_decref(dstr_list *dest, dstr *str);
+
+/**
+ * @brief dstr_list_remove Remove a dynamic string from a list.
+ * @param list The list to remove from.
+ * @param link Link to the dynamic string to remove.
+ */
+void dstr_list_remove(dstr_list *list, dstr_link_t *link);
+
+/**
+ * @brief dstr_list_traverse Traverse a list with a callback.
+ * @param list The list to traverse
+ */
+void dstr_list_traverse(dstr_list * list, void (*callback)(dstr *));
+
+/**
+ * @brief dstr_list_traverse_reverse Traverse a list in reverse with a callback.
+ * @param list The list to traverse
+ */
+void dstr_list_traverse_reverse (dstr_list *list, void (*callback)(dstr *));
+
+/**
+ * Traverse a list with a callback that return 0 or 1 depending on it wants
+ * to delete the element or not.
+ * @param list The list to traverse.
+ */
+void dstr_list_traverse_delete (dstr_list * list, int (*callback)(dstr *));
+
+/**
+ * @brief dstr_list_decref Decrement one reference from string list.
+ * @param list The list to modify.
+ */
+void dstr_list_decref (dstr_list *list);
+
+/**
+ * @brief dstr_list_to_dstr Concat a string list a dynamic string.
+ * @param sep Seperator to seperate each list element. Use 0 if not wanted.
+ * @param list The list to concat.
+ * @return A dynamic string.
+ */
+dstr *dstr_list_to_dstr(const char *sep, dstr_list *list);
+
+#endif /* dstr.h */
