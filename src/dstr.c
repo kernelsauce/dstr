d7d5d2698ce81d13f8343f167fdc22d5cfb7eb86
John Abrahamsen <jab@NOR128.(none)>
2013-01-05 12:53:10 +0100

    removed duplicate str->mem set

--- src/dstr.c
+++ src/dstr.c
@@ -612,7 +612,6 @@ int dstr_resize_fill(dstr *str, size_t n, char fill)
     } else {
         if (!__dstr_alloc(str, n_with_sz))
             return 0;
-        str->mem = n_with_sz;
         __dstr_safe_memset(str->data + (str->sz), fill, n);
         str->sz = n;
         str->data[str->sz + 1] = '\0';
